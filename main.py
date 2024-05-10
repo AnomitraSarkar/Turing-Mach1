3a635ab0fe1b7eafe123acc766a40ee103fa3d4f
Anomitra Sarkar <anomitrasarkar05@gmail.com>
2024-05-10 22:35:38 +0530

    parsed machine code

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 DEFAULT_FILENAME = "main.mach"
 
@@ -6,8 +7,23 @@ def read_mach(filename = DEFAULT_FILENAME):
     with open(filename) as f:
         code = f.read()
         return code
-    
 
+def process_code(filename):
+    raw_code = read_mach(filename)
+    split_code = raw_code.split(":")
+    for i in range(0,len(split_code)):
+        split_code[i] = split_code[i].split("\n")
+        if(type(split_code)==list):
+            try:
+                split_code[i] = split_code[i].remove("")
+            except:
+                pass
+    inline_code = []
+    for i in split_code:
+        if( i != None and i != []):
+            for j in i:
+                inline_code.append(j.strip())
+    print(inline_code)
     
 if __name__ == "__main__":
-    read_mach()
+    process_code(DEFAULT_FILENAME)

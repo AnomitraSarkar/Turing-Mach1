e2456adea307afe3b69ebc549d2ad36a2d0cdec0
Anomitra Sarkar <anomitrasarkar05@gmail.com>
2024-05-10 22:47:31 +0530

    made machine class

--- main.py
+++ main.py
@@ -3,6 +3,31 @@ import json
 
 DEFAULT_FILENAME = "main.mach"
 
+class Machine:
+    init = "q0"
+    final="qf"
+    transition=[]
+    tapeStr = '_'
+    tapeList = ["_"]
+    
+    def __init__(self,init,final,trans) -> None:
+        self.init = init;
+        self.final = final
+        self.transition = trans
+    
+    def compile(self)->None:
+       tapeInput = input("Enter Input String Load on Tape: ")
+       self.tape = f"_{tapeInput}_"
+       
+    def concurrentRun(self)->str:
+        self.tapeList = list(self.tapeStr)
+        return "A"
+        
+       
+    
+
+
+
 def read_mach(filename = DEFAULT_FILENAME):
     with open(filename) as f:
         code = f.read()
@@ -13,6 +38,7 @@ def process_code(filename):
     split_code = raw_code.split(":")
     for i in range(0,len(split_code)):
         split_code[i] = split_code[i].split("\n")
+    for i in range(0,len(split_code)):
         if(type(split_code)==list):
             try:
                 split_code[i] = split_code[i].remove("")

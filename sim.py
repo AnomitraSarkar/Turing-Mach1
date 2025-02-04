fa71f4118c078959ab0db9cf05c32f8764baf4e9
AnomitraSarkar <anomitrasarkar05@gmail.com>
2025-02-05 00:00:08 +0530

    hey

--- sim.py
+++ sim.py
@@ -1,12 +1,16 @@
 from playground import Playground
 
-env = Playground()
-env.set_target(point=(200,200))
-env.set_machine(
-    [
+
+machine_config = [
         ((255,0,0), (300,400), (300,300)),
         ((255,0,0), (300,300), (300,200)),
         ((255,0,0), (300,200), (300,100)),
     ]
+
+
+env = Playground()
+env.set_target(point=(200,200))
+env.set_machine(
+    machine_config
 )
 env.run()

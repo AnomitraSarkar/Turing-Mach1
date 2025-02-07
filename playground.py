f65261b3d6114432899fa4803836d15687ec98b4
AnomitraSarkar <anomitrasarkar05@gmail.com>
2025-02-08 00:31:26 +0530

    csv

--- playground.py
+++ playground.py
@@ -12,9 +12,14 @@ def append_file(writer, headers, data):
     file_exists = False
     try:
         with open(writer, 'r') as csvfile:
-            file_exists = bool(csvfile.readline())
+            rows = list(csv.reader(csvfile))
+            file_exists = bool(rows)
+            last_row = rows[-1] if rows else None
     except FileNotFoundError:
-        pass
+        last_row = None
+    
+    if last_row and last_row == data:
+        return  # Avoid appending duplicate last value
     
     with open(writer, 'a', newline='') as csvfile:
         csv_writer = csv.writer(csvfile)
@@ -89,6 +94,11 @@ class Playground():
             self.screen.fill(WHITE)
             self.init_target()
             self.init_machine()
+            # append_file(
+            #     f"target-{self.target}-def-mach-config.csv",
+            #     ["X0","Y0","X1","Y1","XF","YF","Angle0", "Angle1", "TargetX", "TargetY"],
+            #     [self.geometry[0][1][0],self.geometry[0][1][1], self.geometry[1][1][0],self.geometry[1][1][1], self.geometry[2][1][0],self.geometry[2][1][1], self.angles[0], self.angles[1], self.target[0], self.target[1]]
+            #     )
             pygame.display.update()
         
         pygame.quit()
